95093ce62a71394279dce980cd065c338adcc354
egalamback <egalamback@gmail.com>
2024-09-27 16:33:20 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,1 @@
-variable "resource_group_name" {
-  description = "The name of the resource group containing the state"
-  type        = string
-}
 variable "location" {}
-variable "storage_account_name" {
-  description = "The name of the storage account containing the state"
-  type        = string
-}
